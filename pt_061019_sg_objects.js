bc01de4d7e14216d290a62d3f34f77b71a5fb7ec
betalantz <betalantz@gmail.com>
2019-10-14 09:18:31 -0700

    completes custom rails obj and moves to new file

---
+++ pt_061019_sg_objects.js
@@ -0,0 +1,164 @@
+// pt-061019 Rails Objects
+
+let railsSection = [
+    {
+        categories: ['Introduction to Rails', 'Intro to Rails', 'Rails Application Basics', 'Rails MVC', 'Rails Static Request', 'Rails Hello World'],
+        title: `${cohort} Live Lecture: Intro to Rails`,
+        description: 'Introduction to the popular Rails framework (Ruby on Rails), its similarities and differences from Sinatra',
+        day: day1, 
+        time: time1,
+        weekToStart: studygroupWeeks[0],
+        weekToEnd: studygroupWeeks[0],
+        zoom
+    },
+    {
+        categories: ['Rails Models Basics', 'ActiveRecord Models and Rails', 'Intro to REST', 'RESTful Action Index Lab', 'Rails Dynamic Request Lab'],
+        title: `${cohort} Open Office Hours`,
+        description: 'Bring your questions about Models and RESTful routes in Rails. We will do some hands-on exploration of these topics.',
+        day: day2, 
+        time: time2,
+        weekToStart: studygroupWeeks[0],
+        weekToEnd: studygroupWeeks[0],
+        zoom
+    },
+    {
+        categories: ['Rails MVC', "CRUD with Rails",'Rails URL Helpers', 'Rails form_tag', 'Create Action', 'Index, Show, New, Create Lab', 'Edit/Update Action', 'form_for on Edit', 'Rails Generator', 'Rails Blog Scaffold'],
+        title: `${cohort} Live Lecture: CRUD Operations in Rails`, 
+        description: 'Learn how to perform CRUD actions using Ruby on Rails.',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[1],
+        weekToEnd: studygroupWeeks[1],
+        zoom
+    },
+    {
+        categories: ['Rails MVC', "CRUD with Rails"],
+        title: `${cohort} Open Office Hours`, 
+        description: 'Bring your questions about CRUD in Ruby on Rails and we will learn by doing some group coding.',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[1],
+        weekToEnd: studygroupWeeks[1],
+        zoom
+    },
+    {
+        categories: ['Associations and Rails', 'Rails Forms Overview'],
+        title: `${cohort} Live Lecture: Rails Associations and Nested Forms`, 
+        description: 'Understand model relations and forms in Ruby on Rails',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[2],
+        weekToEnd: studygroupWeeks[2],
+        zoom
+    },
+    {
+        categories: ['Associations and Rails', 'Rails Forms Overview'],
+        title: `${cohort} Open Office Hours`, 
+        description: 'Bring your questions about mode relations and forms in Ruby on Rails and we will learn by doing some group coding.',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[2],
+        weekToEnd: studygroupWeeks[2],
+        zoom
+    },
+    {
+        categories: ['Layouts and Templates in Rails'],
+        title: `${cohort} Live Lecture: Refactoring with Layouts, Partials, Helpers`, 
+        description: 'Gain more control over you view layouts in Ruby on Rails',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[3],
+        weekToEnd: studygroupWeeks[3],
+        zoom
+    },
+    {
+        categories: ['Layouts and Templates in Rails'],
+        title: `${cohort} Open Office Hours`, 
+        description: 'Bring your questions about view layouts and templates in Ruby on Rails and we will learn by doing some group coding.',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[3],
+        weekToEnd: studygroupWeeks[3],
+        zoom
+    },
+    {
+        categories: ['Routes and Resources'],
+        title: `${cohort} Live Lecture: Routing in Rails - Nested Routes`, 
+        description: 'Learn about routing and nested routes in Ruby on Rails ',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[4],
+        weekToEnd: studygroupWeeks[4],
+        zoom
+    },
+    {
+        categories: ['Routes and Resources'],
+        title: `${cohort} Open Office Hours`, 
+        description: 'Bring your questions about routing and nested routes in Ruby on Rails and we will learn by doing some group coding.',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[4],
+        weekToEnd: studygroupWeeks[4],
+        zoom
+    },
+    {
+        categories: ['Authentication'],
+        title: `${cohort} Live Lecture: Authentication in Rails`,
+        description: 'Learn about authentication, cookies and sessions in the Ruby on Rails Framework',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[5], 
+        weekToEnd: studygroupWeeks[5],
+        zoom
+    },
+    {
+        categories: ['Authentication'],
+        title: `${cohort} Open Office Hours`,
+        description: 'Bring your questions about authentication, cookies and sessions in Ruby on Rails and we will learn by doing some group coding.',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[5], 
+        weekToEnd: studygroupWeeks[5],
+        zoom
+    },
+    {
+        categories: ['Rails Project Mode'],
+        title: `${cohort} Rails Portfolio Project Prep`,
+        description: 'Preparation for your Rails Portfolio Project',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[6],
+        weekToEnd: studygroupWeeks[6],
+        zoom
+    },
+    {
+        categories: ['Rails Project Mode'],
+        title: `${cohort} Open Office Hours`,
+        description: 'Bring your questions and we will learn by doing some group coding.',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[6],
+        weekToEnd: studygroupWeeks[6],
+        zoom
+    },
+    {
+        categories: ['Rails Project Mode'],
+        title: `${cohort} Open Office Hours (Rails Portfolio Project)`,
+        description: 'Open Office Hours for your Rails Portfolio Project',
+        day: day1,
+        time: time1,
+        weekToStart: studygroupWeeks[7],
+        weekToEnd: studygroupWeeks[7],
+        zoom
+    },
+    {
+        categories: ['Rails Project Mode'],
+        title: `${cohort} Open Office Hours (Rails Portfolio Project)`,
+        description: 'Open Office Hours for your Rails Portfolio Project',
+        day: day2,
+        time: time2,
+        weekToStart: studygroupWeeks[7],
+        weekToEnd: studygroupWeeks[7],
+        zoom
+    }
+]
